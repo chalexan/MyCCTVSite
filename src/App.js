8467ff56ac0bc7f8e89a406327477d64e269a0a0
chalexan <cherednychenko.alexandr@gmail.com>
2022-05-19 17:17:58 +0300

    init-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'antd/dist/antd.css'
+import LayoutPage from './components/LayoutPage';
+
+function App() {
+  return (
+    <div>
+      <LayoutPage />
+    </div>
+  );
+}
+
+export default App;
