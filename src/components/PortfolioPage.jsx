2eb406fc714106b3f1204c0613ce3c5297694ae0
chalexan <cherednychenko.alexandr@gmail.com>
2022-05-20 12:05:20 +0300

    add-feedback-and-portfolio

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -1,5 +1,109 @@
+import { Card, Image } from "antd";
+const { Meta } = Card;
+
 const PortfolioPage = () => {
-  return <div>PortfoloPage!</div>;
+  const gridStyle = {
+    width: "25%",
+    textAlign: "center",
+  };
+
+  return (
+    <>
+      <Card title="Наши работы">
+        <Card.Grid
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card.Grid>
+        <Card.Grid
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card.Grid>
+        <Card.Grid
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card.Grid>
+        <Card.Grid
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card.Grid>
+        <Card.Grid
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card.Grid>
+        <Card.Grid
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card.Grid>
+        <Card.Grid
+          hoverable
+          style={{ width: 240 }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          <Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card.Grid>
+      </Card>
+    </>
+  );
 };
 
 export default PortfolioPage;
